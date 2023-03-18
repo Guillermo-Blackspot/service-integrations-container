4827142cedd951f9be568cbae9301292a85b90fc
Guillermo-Rodriguez <guillermo.blackspot@gmail.com>
2023-03-18 15:39:28 -0600

    Optimizaciones

--- src/Concerns/ServiceIntegrationFinder.php
+++ src/Concerns/ServiceIntegrationFinder.php
@@ -4,51 +4,188 @@ namespace BlackSpot\ServiceIntegrationsContainer\Concerns;
 
 use BlackSpot\ServiceIntegrationsContainer\Models\ServiceIntegration;
 use BlackSpot\ServiceIntegrationsContainer\ServiceProvider;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use LogicException;
 
 trait ServiceIntegrationFinder
 {   
+    protected $serviceIntegrationsFound = [];
+    protected $resolverMethodFound = false;
+
     /**
      * Build the service integration query to find
      * 
      * @param int|null $serviceIntegrationId
+     * @param array $resolverMethods[columnResolver, idResolver]
      * 
-     * @return \Illuminate\Database\Query\Builder|null
+     * @return \Illuminate\Database\Query\Builder
      */
-    protected function getServiceIntegrationQueryFinder($serviceIntegrationId = null, $resolverMethods = [])
+    protected function getServiceIntegrationQueryFinder($serviceIntegrationId = null, $provider = null)
     {
-        $serviceIntegrationModel     = ServiceProvider::getFromConfig('model', ServiceIntegration::class);
-        $serviceIntegrationTableName = $serviceIntegrationModel::TABLE_NAME;
-        $query                       = DB::table($serviceIntegrationTableName);
+        $serviceIntegrationClass = ServiceProvider::getFromConfig('model', ServiceIntegration::class);
 
-        if (!is_null($serviceIntegrationId)) {
-            $query = $query->where('id', $serviceIntegrationId);
-        }else if (method_exists($this, 'getMainServiceIntegrationId')){
-            $query = $query->where('id', $this->getMainServiceIntegrationId());
-        }else if (method_exists($this, 'getMainServiceIntegrationOwnerId') && method_exists($this,'getMainServiceIntegrationOwnerType')){
-            $query = $query->where('owner_type', $this->getMainServiceIntegrationOwnerType())->where('owner_id', $this->getMainServiceIntegrationOwnerId());
-        }else if(is_array($resolverMethods) && $resolverMethods != []){
-            if (method_exists($this, $resolverMethods[0]) && method_exists($this, $resolverMethods[1])) {
-                $query = $query
-                            ->where('owner_id', $this->{$resolverMethods[0]}())
-                            ->where('owner_type', $this->{$resolverMethods[1]}());
-            }else{
-                $query = $query->where('owner_type', 'not-exists-expecting-null');
-            }
-        }else if(is_string($resolverMethods) && $resolverMethods){
-            if (method_exists($this, $resolverMethods)) {
-                $query = $query->where('id', $this->{$resolverMethods}());
-            }else{
-                $query = $query->where('id', 'not-exists-expecting-null');
-            }
-        }elseif (isset($this->id) && self::class == $serviceIntegrationModel) {
-            $query = $query->where('id', $this->id);
-        }else if (isset($this->service_integration_id)){
-            $query = $query->where('id', $this->service_integration_id);
-        }else{
-            $query = $query->where('id', 'not-exists-expecting-null');
+        /* 
+         * Evaluates if the "service_integration" relationship exists,
+         * 
+         * if exists return the query
+         */
+
+        $belongsTo = $this->queryFromBelongsToRelationship();
+
+        if (! is_null($belongsTo)) {
+            return $belongsTo;
+        }
+
+        /*
+         * Try to resolve the service integration to find
+         * 
+         * by id or by the resolver methods
+         */
+        $query = ServiceIntegration::query();
+
+        $this->whereIdCondition($query, $serviceIntegrationId, $provider)
+            ->whereCustomColumCondition($query, $provider);
+
+
+        if (! $this->resolverMethodFound) {
+            $query->where('id', 'defined-resolver-not-found');
         }
 
         return $query;
+    }
+
+
+    public function resolveServiceIntegrationFromInstance($instance, $serviceIntegrationId)
+    {
+        // Is the ServiceIntegration Model
+        if (isset($instance) && get_class($instance) == ServiceProvider::getFromConfig('model', ServiceIntegration::class)) {
+            if (! is_null($serviceIntegrationId) && $this->id == $serviceIntegrationId) {                
+                return $instance;
+            }
+        }
+        // If the relationships is loaded
+        else if (method_exists($instance, 'service_integration') && $instance->relationLoaded('service_integration')) {
+
+            if ($instance->service_integration->id != $serviceIntegrationId) {
+                throw new LogicException('The relationship "service_integration" cannot be different at the foreign_key.');    
+            }
+
+            return $instance->service_integration;
+        }
+    }
+
+    /**
+     * Determine if the given service integration id was already found
+     *
+     * @param int $serviceIntegrationId
+     * @return bool
+     */
+    public function serviceIntegrationWasLoaded($serviceIntegrationId)
+    {
+        return isset($this->serviceIntegrationsFound[$serviceIntegrationId]);
+    }
+
+    /**
+     * Get the given service integration found
+     *
+     * @param int $serviceIntegrationId
+     * @return \BlackSpot\ServiceIntegrationsContainer\Models\ServiceIntegration
+     */
+    public function getServiceIntegrationLoaded($serviceIntegrationId)
+    {
+        return $this->serviceIntegrationsFound[$serviceIntegrationId];
+    }
+
+    public function putServiceIntegrationFound($serviceIntegration)
+    {
+        return $this->serviceIntegrationsFound[$serviceIntegration->id] = $serviceIntegration;
+    }
+
+
+    /**
+     * Set where id condition
+     *
+     * @param \Illuminate\Database\Eloquent\Query  $query
+     * @param int  $serviceIntegrationId
+     * @param string  $provider
+     * @return $this
+     */
+    private function whereIdCondition(&$query, $serviceIntegrationId, $provider = null)
+    {
+        if ($this->resolverMethodFound) {
+            return $this;
+        }
+        
+        // Specific id
+        if (! is_null($serviceIntegrationId)) {
+            $serviceIntegrationId = (int) $serviceIntegrationId;
+        }
+
+        // Determine if is a instance of ServiceIntegration
+        else if (isset($this->id) && self::class == ServiceProvider::getFromConfig('model', ServiceIntegration::class)) {
+            $serviceIntegrationId = (int) $this->id;
+        }
+
+        // Try to resolve from the getServiceIntegrationResolvers method
+        else if (method_exists($this, 'getServiceIntegrationResolvers') && ! is_null($provider) && Arr::has(($resolverMethods = $this->getServiceIntegrationResolvers()), "{$provider}.idResolver")) {            
+            $serviceIntegrationId = (int) $resolverMethods[$provider]['idResolver'];
+        }
+        
+        // Ignoring next resolvers
+        if ($serviceIntegrationId) {
+            $this->resolverMethodFound = true;
+            $query->where('id', $serviceIntegrationId)
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set where id condition
+     *
+     * @param \Illuminate\Database\Eloquent\Query  $query
+     * @param string  $provider
+     * @return $this
+     */
+    private function whereCustomColumCondition(&$query, $provider = null)
+    {
+        if ($this->resolverMethodFound) {
+            return $this;
+        }
+
+        $conditions = [];
+
+        // Try to resolve from the getServiceIntegrationResolvers method
+        if (method_exists($this, 'getServiceIntegrationResolvers') && ! is_null($provider)) {
+            if (Arr::has(($conditions = $this->getServiceIntegrationResolvers()), "{$provider}.columnResolver")) {            
+                foreach ($conditions[$provider]['columnResolver'] as $condition) {
+                    $query->where($condition[0], $condition[1], $condition[2]);
+                }
+            }
+        }
+
+        if (! empty($conditions)) {
+            $this->resolverMethodFound = true;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Check if service_integration relationship exists
+     *
+     * @return \Illuminate\Database\Eloquent\Query\Builder
+     */
+    private function queryFromBelongsToRelationship()
+    {                
+        if (method_exists($this, 'service_integration')) {
+            return $this->service_integration()->getQuery();
+        }
+    
+        // Relationship not defined but the service_integration_id is present
+        if (isset($this->service_integration_id)) {
+            throw new LogicException("The belongsTo \"service_integration\" relationship not defined.", 1);                
+        }
     }
 }
